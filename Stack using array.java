de941ece86c0d8ff0df58ddf937153659ad053e1
nishantruwari <83151342+nishantruwari@users.noreply.github.com>
2025-03-30 22:30:57 +0530

    Create Stack using array.java

---
+++ Stack using array.java
@@ -0,0 +1,24 @@
+class MyStack {
+    private int[] arr;
+    private int top;
+
+    public MyStack() {
+        arr = new int[1000];
+        top = -1;
+    }
+
+    public void push(int x) {
+        arr[++top]=x;
+        // Your Code
+    }
+
+    public int pop() {
+        
+        if(top==-1){
+            return -1;
+        }
+        top=top-1;
+        return arr[top+1];
+        // Your Code
+    }
+}
